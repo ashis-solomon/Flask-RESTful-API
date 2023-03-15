c332bb16b84b7fa1c6f9f5abb5d9f49515bc6e20
ashis-solomon <ashissolomon24@gmail.com>
2023-03-16 01:12:23 +0530

    added tests

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BASE_URL = 'http://127.0.0.1:5000/'
+    BASE_URL = 'http://127.0.0.1:5000'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-secret-key'
     DB_NAME = 'db'
     MONGODB_URI = os.environ.get('MONGODB_URI') or 'mongodb://localhost:27017/db'
