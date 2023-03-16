698a31d4c9151b05a4af023b06f2d65b9d067fd9
ashis-solomon <ashissolomon24@gmail.com>
2023-03-16 11:56:03 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, marshal_with, fields
 from pymongo import MongoClient
 from bson import ObjectId
 
@@ -10,7 +10,15 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['db']
 users_collection = db['users_db']
 
+user_fields = {
+    '_id': fields.String,
+    'name': fields.String,
+    'email': fields.String,
+    'password': fields.String
+}
+
 class User(Resource):
+    @marshal_with(user_fields)
     def get(self, id=None):
         if id:
             user = users_collection.find_one({'_id': ObjectId(id)})
@@ -26,17 +34,22 @@ class User(Resource):
                 users.append(user)
             return users
 
+    @marshal_with(user_fields)
     def post(self):
         data = request.json
         result = users_collection.insert_one(data)
-        user_id = str(result.inserted_id)
-        return {'id': user_id}, 201
+        user = users_collection.find_one({'_id': result.inserted_id})
+        user['_id'] = str(user['_id'])
+        return user, 201
 
+    @marshal_with(user_fields)
     def put(self, id):
         data = request.json
         result = users_collection.update_one({'_id': ObjectId(id)}, {'$set': data})
+        user = users_collection.find_one({'_id': ObjectId(id)})
         if result.matched_count == 1:
-            return {'message': 'User updated successfully'}, 200
+            user['_id'] = str(user['_id'])
+            return user, 200
         else:
             return {'error': 'User not found'}, 404
 
